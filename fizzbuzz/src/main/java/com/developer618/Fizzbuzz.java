417454b2561786650e39c8199894a42b102068a9
618developer <618developer@gmail.com>
2022-01-13 00:29:50 -0600

    Updated logic for modulus symbol. Added new tests to drive the algorithm to check all conditions for 3 and 5.

--- fizzbuzz/src/main/java/com/developer618/Fizzbuzz.java
+++ fizzbuzz/src/main/java/com/developer618/Fizzbuzz.java
@@ -2,14 +2,14 @@ package com.developer618;
 
 public class Fizzbuzz {
     public static String printResults(int number) {
-        if(number == 3) {
+        if(number % 15 == 0) {
+            return "Fizzbuzz";
+        }
+        if(number % 3 == 0) {
             return "Fizz";
         }
-        if(number == 5) {
+        if(number % 5 == 0) {
             return "Buzz";
-        }
-        if(number == 15) {
-            return "Fizzbuzz";
         }
         return Integer.toString(number);
     }
--- fizzbuzz/src/test/java/com/developer618/FizzbuzzTest.java
+++ fizzbuzz/src/test/java/com/developer618/FizzbuzzTest.java
@@ -8,6 +8,11 @@ import static org.hamcrest.Matchers.is;
 
 public class FizzbuzzTest {
 
+    private int getRandomNumber(int min, int max, int divisibleBy) {
+        return (int)((Math.random() * (max - min)) + min) * divisibleBy;
+    }
+
+    //First Set of tests
     @Test
     public void printNumberNotDividedBy3Or5() {
         assertThat(Fizzbuzz.printResults(1), is(equalTo("1")));
@@ -26,5 +31,21 @@ public class FizzbuzzTest {
     @Test
     void printFizzBuzzForNumberDividedBy3And5() {
         assertThat(Fizzbuzz.printResults(15), is(equalTo("Fizzbuzz")));
+    }
+
+    //Second set of tests that drove the algorithm to have the % used
+    @Test
+    void printFizzForAnyRandomNumberDividedBy3() {
+        assertThat(Fizzbuzz.printResults(getRandomNumber(1,4,3)), is(equalTo("Fizz")));
+    }
+
+    @Test
+    void printBuzzForAnyRandomNumberDividedByOnly5() {
+        assertThat(Fizzbuzz.printResults(getRandomNumber(10,11,5)), is(equalTo("Buzz")));
+    }
+
+    @Test
+    void printFizzBuzzForAnyRandomNumberDividedByOnly15() {
+        assertThat(Fizzbuzz.printResults(getRandomNumber(1, 6, 15)), is(equalTo("Fizzbuzz")));
     }
 }
