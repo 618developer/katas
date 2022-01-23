e65c53c5a017c6cc0a9a479ea67212eb85cdd0db
618developer <618developer@gmail.com>
2022-01-23 14:17:24 -0600

    Finalize ReplaceNth algorithm

--- ReplaceNth/src/main/java/com/developer618/ReplaceNth.java
+++ ReplaceNth/src/main/java/com/developer618/ReplaceNth.java
@@ -3,6 +3,18 @@ package com.developer618;
 public class ReplaceNth {
 
     public static String replaceNth(String text, int n, char oldValue, char newValue) {
-        return "";
+        int occurrences = 0;
+        StringBuilder updatedText = new StringBuilder();
+        for (char charInString :
+                text.toCharArray()) {
+            if (oldValue == charInString) {
+                occurrences++;
+                if(occurrences % n == 0) {
+                    charInString = newValue;
+                }
+            }
+            updatedText.append(charInString);
+        }
+        return updatedText.toString();
     }
 }
--- ReplaceNth/src/test/java/com/developer618/ReplaceNthTest.java
+++ ReplaceNth/src/test/java/com/developer618/ReplaceNthTest.java
@@ -1,5 +1,6 @@
 package com.developer618;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -9,11 +10,41 @@ import static org.hamcrest.Matchers.is;
 
 public class ReplaceNthTest {
 
+    private final String originalText = "Vader said: No, I am your father!";
+
+    @DisplayName("Let's handle the 0 for n first, original value returned")
+    @Test
+    void handleZeroPassedIn() {
+        // Arrange
+
+        // Act
+        // Assert
+        assertThat(ReplaceNth.replaceNth(originalText, 0, 'x', 'y'), is(equalTo(originalText)));
+    }
+
+    @DisplayName("If n is negative, original value returned")
+    @Test
+    void handleNegativeNPassedIn() {
+        // Arrange
+        // Act
+        // Assert
+        assertThat(ReplaceNth.replaceNth(originalText, -1, 'x', 'y'), is(equalTo(originalText)));
+    }
+
+    @DisplayName("N being 1 will replace every occurence.")
+    @Test
+    void handleNBeing1_replaceAllAsWithB() {
+        // Arrange
+        String replaceAllAsSolution = "Vbder sbid: No, I bm your fbther!";
+        // Act
+        // Assert
+        assertThat(ReplaceNth.replaceNth(originalText, 1, 'a', 'b'), is(equalTo(replaceAllAsSolution)));
+    }
+
     @Test
     void solutionTest_2ndOccurence_replaceAwithO() {
         String solution = "Vader soid: No, I am your fother!";
-        String original = "Vader said: No, I am your father!";
 
-        assertThat(ReplaceNth.replaceNth(original, 2, 'a', 'o'), is(equalTo(solution)));
+        assertThat(ReplaceNth.replaceNth(originalText, 2, 'a', 'o'), is(equalTo(solution)));
     }
 }
