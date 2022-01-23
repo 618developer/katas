8134759b4867f6486f38870d88b0c581d8f7236e
618developer <618developer@gmail.com>
2022-01-22 22:30:55 -0600

    Updated logic for modulus symbol. Added new tests to drive the algorithm to check all conditions for 3 and 5.

--- fizzbuzz/src/main/java/com/developer618/Fizzbuzz.java
+++ fizzbuzz/src/main/java/com/developer618/Fizzbuzz.java
@@ -2,13 +2,13 @@ package com.developer618;
 
 public class Fizzbuzz {
     public static String printResults(int number) {
-        if(number % 15 == 0) {
+        if (number % 15 == 0) {
             return "Fizzbuzz";
         }
-        if(number % 3 == 0) {
+        if (number % 3 == 0) {
             return "Fizz";
         }
-        if(number % 5 == 0) {
+        if (number % 5 == 0) {
             return "Buzz";
         }
         return Integer.toString(number);
